7095a707d21d3849a34b6b3ef9a5509b816fdd48
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-06 19:20:40 +0500

    10. Creating the database using Entity Framework Code first migrations

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2628ca90391925173eaf21667087f64dd57a9da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775bd0e9385fa6a5b4fdcac26eed8ac2b9976096")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
