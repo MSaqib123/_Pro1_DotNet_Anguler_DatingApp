d8eb3e17a412c0bfe1421efe6e316622c1e6866f
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-07 08:01:55 +0500

    4. Adding VS Code extensions to work with Angular
    5. Making HTTP requests in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  http = inject(HttpClient); 
   title = 'client';
+  users:any;
 
-  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/user').subscribe({
+      next:response=> this.users = response,
+      error:error=>console.log(error),
+      complete:()=> console.log("complete the requiest")
+    });
+  }
 }
