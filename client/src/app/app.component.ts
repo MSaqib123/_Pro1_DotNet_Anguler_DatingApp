e807f33a458002865c45427198db749bd2b5a774
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-07 11:13:30 +0500

    5. Making HTTP requests in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
