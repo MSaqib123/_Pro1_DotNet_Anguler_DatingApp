eccc984dac27b594850fccee9ff782a9b80f0317
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-08 14:59:55 +0500

    abc

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1724dafa205c0de254793bab0c5c959ba39f83fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6687c1e329b3c8c85cb371da709357d5392c2179")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
