464c6486315b6f97296f3daece41c32216c21494
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-09 00:25:31 +0500

    4. Routing in code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,7 +3,7 @@ import {FormsModule} from "@angular/forms";
 import { AccountService } from '../_services/account.service';
 import { CommonModule } from '@angular/common';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterLink,RouterLinkActive  } from '@angular/router';
+import { Router, RouterLink,RouterLinkActive  } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -13,14 +13,15 @@ import { RouterLink,RouterLinkActive  } from '@angular/router';
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   model:any = {};
   login(){
     //console.log(this.model);
     // ===============
     // Abservable  we have to subscribe to abservable because they are lazzy
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response,"api Response");
+      next: _ => {
+        void this.router.navigateByUrl('/members');
       },
       error: error=> console.log(error),
       complete: () => {
@@ -31,5 +32,6 @@ export class NavComponent {
 
   logout(){
     this.accountService.logout();
+    void this.router.navigateByUrl('/');
   }
 }
