4f5020793f514670e7552b4ab943430185615ed5
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-08 09:49:03 +0500

    5. Injecting services into components

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8e58e30ea93c7bf7253cfb45f0bb986535b75a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22876e94c6d1742cbd99e62ab8077ee838acb836")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   imports: [FormsModule],
@@ -7,9 +8,23 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService);
   model:any = {};
+  loggedIn = false;
 
   login(){
-    console.log(this.model);
+    //console.log(this.model);
+    // ===============
+    // Abservable  we have to subscribe to abservable because they are lazzy
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response,"api Response");
+        this.loggedIn = true;
+      },
+      error: error=> console.log(error),
+      complete: () => {
+        console.log("✅ Request completed successfully");
+      }
+    });
   }
 }
