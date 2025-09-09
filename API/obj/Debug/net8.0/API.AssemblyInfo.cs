9f129c4f4b6b4a22d446f465f7f762f90db26872
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-09 16:32:56 +0500

    6. Adding an error interceptor

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1f011d0b7e5627db0abc4736c10fe6d666544a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65e2273824eb4f9c12dff614d1c3f99047552b83")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
