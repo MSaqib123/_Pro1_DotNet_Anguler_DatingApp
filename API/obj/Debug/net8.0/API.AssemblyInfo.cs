fdc9cae6b7455b3ebf2fc4223cc90b38e1fce51d
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-18 17:36:53 +0500

    6. Creating member cards

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3605e11cfe37ed50a63820f9c861b42e64a2374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53699736e9eb308d24397418773ad7dccfad1a40")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
