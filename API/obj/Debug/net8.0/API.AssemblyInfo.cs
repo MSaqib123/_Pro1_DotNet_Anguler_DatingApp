1ea19b05e595ec836d2c9871faac6b508b0c9d45
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-18 11:51:26 +0500

    11. Adding a DTO for Members.mp4

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41807aa9804575f3c1a3f217b39479050d10ed5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60146de661ade3f9a72ee42468229a596c898936")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
