1e1596d53f21689cfe978a5ee4d8b54aa5b13fba
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-18 18:40:57 +0500

    9. Using an interceptor to send the token

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -13,17 +13,9 @@ export class MembersService {
   baseUrl = environment.apiUrl;
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
   getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions(){
-    return {
-      headers: {
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
-      }
-    }
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { errorInterceptor } from './_interceptors/error.interceptor'; // This path must be correct
+import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor,jwtInterceptor])),
   ],
 };
 
