2dcb6b4b9b3d97addc02d32eb82e113a1768bd04
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-20 22:36:56 +0500

    9. Installing legacy npm packages

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ff7f626c4d6e4ff62155efb2495f1d4baa386c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+accda777ec6edbae81a86eb171446218900904c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,12 +1,16 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Member } from '../../_models/member';
+import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { AccountService } from '../../_services/account.service';
 
 @Component({
   selector: 'app-photo-editor',
-  imports: [],
+  imports: [NgIf,NgFor, NgStyle, NgClass],
   templateUrl: './photo-editor.component.html',
   styleUrl: './photo-editor.component.css'
 })
 export class PhotoEditorComponent {
+  private accountService = inject(AccountService);
   member = input.required<Member>();
+  //uploader?: FileUploader;
 }
