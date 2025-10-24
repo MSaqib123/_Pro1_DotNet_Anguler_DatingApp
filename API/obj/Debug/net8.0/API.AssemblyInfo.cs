5e81dcc25b3c127799cfc593103477d3b5a51861
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-24 13:17:09 +0500

    8. Adding the lists component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff521c9343fd76eef541293391d97b35b631821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+522dc454e3da3d075ee846975e4d1be6598a8ba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
