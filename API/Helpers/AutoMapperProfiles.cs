43b4e68da00e05ea2e5d8148a596e177ef35d2d5
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-26 19:16:19 +0500

    4. Setting up the automapper profiles

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -22,7 +22,13 @@ namespace API.Helpers
 
             //Registration Mapper
             CreateMap<RegisterDto, AppUsers>();
-            CreateMap<string, DateOnly>().ConvertUsing(s=>DateOnly.Parse(s));
+            CreateMap<string, DateOnly>().ConvertUsing(s => DateOnly.Parse(s));
+
+            CreateMap<Message, MessageDto>()
+                .ForMember(d => d.SenderPhotoUrl,
+                    o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain)!.Url))
+                .ForMember(d => d.RecipientPhotoUrl,
+                    o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain)!.Url));
         }
     }
 }
