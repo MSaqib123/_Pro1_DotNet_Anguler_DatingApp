05f6198878be65f14f05748d74214303b6b13b20
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-26 15:50:44 +0500

    10. Paginating the likes on the client

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,24 +1,26 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { LikesService } from '../_services/likes.service';
 import { Member } from '../_models/member';
 import { MemberCardComponent } from "../members/member-card/member-card.component";
 import { FormsModule } from '@angular/forms';
+import { NgbPagination } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-lists',
-  imports: [MemberCardComponent,FormsModule],
+  imports: [MemberCardComponent, FormsModule, NgbPagination],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent implements OnInit {
-  private likesService = inject(LikesService);
-  members : Member[] = [];
+export class ListsComponent implements OnInit, OnDestroy {
+  likesService = inject(LikesService);
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
 
   ngOnInit(): void {
     this.loadLikes();
   }
-
+  
   getTitle(){
     switch(this.predicate){
       case 'liked':return 'Members you like';
@@ -28,9 +30,30 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes(){
-    this.likesService.getLikes(this.predicate).subscribe({
-      next: members => this.members = members
-    })
+    this.likesService.getLikes(this.predicate,this.pageNumber,this.pageSize);
+  }
+
+
+  pageChanged(page:number){    
+    console.log(page)
+    if(this.pageNumber !== page){
+      this.pageNumber = page;
+      this.loadLikes();
+    }
+
+
+
+    // if(this.memberService.userParams().pageNumber !== page)
+    // {
+    //   this.memberService.userParams().pageNumber = page;
+    //   this.loadMembers();
+    // }
+
+    
+  }
+
+  ngOnDestroy(): void {
+    this.likesService.paginatedResult.set(null);
   }
 
 }
