151171718ba032cebe07eff47062307dcf3f3273
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-30 14:18:40 +0500

    12. Editing user roles

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d868d48a752c16a056e54c2352b1dcd391c6bdfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c7756086633b54ac6b5443e8ff99a195a4dc70")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
