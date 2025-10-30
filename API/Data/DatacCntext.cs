4428aeb79ef40cdccddccd289a0092b2909efd77
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-30 08:16:27 +0500

    3. Configuring the DbContext

--- API/Data/DatacCntext.cs
+++ API/Data/DatacCntext.cs
@@ -1,9 +1,18 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class DataContext(DbContextOptions options) : DbContext(options)
+public class DataContext(DbContextOptions options) : IdentityDbContext<
+                                                    AppUser,
+                                                    AppRole, int,
+                                                    IdentityUserClaim<int>,
+                                                    AppUserRole,
+                                                    IdentityUserLogin<int>,
+                                                    IdentityRoleClaim<int>,
+                                                    IdentityUserToken<int>
+                                                    >(options)
 {
-    public DbSet<AppUser> Users{ get; set; }
     public DbSet<UserLike> Likes { get; set; }
     public DbSet<Message> Messages { get; set; }
 
@@ -15,6 +24,24 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 
 
         // Customizing the Keys of tables 
+
+
+
+        //===========
+        //=========== AppUser and AppRole Relation ===========
+        //===========
+        builder.Entity<AppUser>()
+            .HasMany(ur => ur.UserRoles)
+            .WithOne(u => u.User)
+            .HasForeignKey(ur => ur.UserId)
+            .IsRequired();
+
+        builder.Entity<AppRole>()
+            .HasMany(ur => ur.UserRoles)
+            .WithOne(u => u.Role)
+            .HasForeignKey(ur => ur.RoleId)
+            .IsRequired();
+
 
         //===========
         //=========== UserLike Relation ===========
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213d46ae23628ffc30721d6ecfd628ba864bbee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f499040457906198b8a6f6b569ff299387a35d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
