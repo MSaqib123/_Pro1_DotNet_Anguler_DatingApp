483394bc92ffa5e8cf245fb5fb41d55d1633809a
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-30 09:52:59 +0500

    8. Adding roles to the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -10,7 +10,7 @@ namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync()) return;
 
@@ -20,12 +20,39 @@ public class Seed
             PropertyNameCaseInsensitive = true
         };
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options)!;
-        
+        if (users == null) return;
+
+        var roles = new List<AppRole>
+        {
+            new() {Name = "Member"},
+            new() {Name = "Admin"},
+            new() {Name = "Moderator"},
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+
+
         foreach (var user in users)
         {
             user.UserName = user.UserName!.ToLower();
             await userManager.CreateAsync(user, "Saqib12345.");
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        var admin = new AppUser
+        {
+            UserName = "admin",
+            KnownAs = "Admin",
+            Gender = "",
+            City = "",
+            Country = "",
+        };
+
+        await userManager.CreateAsync(admin, "Saqib12345.");
+        await userManager.AddToRolesAsync(admin, ["Admin","Moderator"]);
     }
 }
 
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c1cabd8ecaff57c2d9bec92143e98fa8262c66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a9e4824fd2071782bb04a853ba42ee7b679f008")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
