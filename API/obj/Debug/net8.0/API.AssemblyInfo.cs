406ccc37247e036ac18ccd02767646d9b008a913
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-04 10:37:01 +0500

    4. Creating a Sql Server instance

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7de94d371d6c43c21263204135942864b49e3ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8562691f5e4ccf370aee7087280515c77b2d33")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
